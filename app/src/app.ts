a16feec6a600c30617064cc3635aaa2cf187696d
andratcreo <andrzej.ratajski@effiware.com>
2022-11-17 18:11:37 +0100

    Bugfixes

--- app/src/app.ts
+++ app/src/app.ts
@@ -18,6 +18,9 @@ const LOGIN_WHEN_NO_TOKEN = ["true", "True", "1"].includes(
 );
 const COOKIE_NAME = process.env.COOKIE_NAME || "cx_forward_auth";
 
+const loginCache = new NodeCache({ stdTTL: 10 * 60 });
+let oidcClient: BaseClient;
+
 const app: Application = express();
 if (LOGIN_WHEN_NO_TOKEN) {
   app.use(
@@ -39,10 +42,10 @@ app.use(express.static("public"));
 app.set("views", "public");
 app.set("view engine", "ejs");
 
-const loginCache = new NodeCache({ stdTTL: 10 * 60 });
-let oidcClient: BaseClient;
-
-// middleware to test if authenticated via session cookie
+/**
+ * General session-based authentication middleware. It allows the resource
+ * if session cookie is present and not expired.
+ */
 const isSessionEstablished = (
   req: Request,
   res: Response,
@@ -55,6 +58,10 @@ const isSessionEstablished = (
   }
 };
 
+/**
+ * Main endpoint for session-based authentication. If session check fails,
+ * request is handled by the _other_ '/' endpoint
+ */
 app.get(
   "/",
   isSessionEstablished,
@@ -71,55 +78,78 @@ app.get(
   }
 );
 
-app.get("/", async (req: Request, res: Response): Promise<void> => {
-  logger.debug(`Call to '/' from ${req.ip}`);
-  if (req.headers.authorization) {
-    const authHeader = req.headers.authorization.split(" ");
-    if (authHeader.length != 2) {
-      res.status(401).render("401/index.ejs");
+/**
+ * Main endpoint for token-based authentication. If LOGIN_WHEN_NO_TOKEN is true
+ * it also invokes OIDC Client to get authorization code.
+ */
+app.get(
+  "/",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    logger.debug(`Call to '/' from ${req.ip}`);
+    if (req.headers.authorization) {
+      const authHeader = req.headers.authorization.split(" ");
+      if (authHeader.length != 2) {
+        res.status(401).render("401/index.ejs");
+        return;
+      } else if (authHeader[0] !== "Bearer" && authHeader[0] !== "bearer") {
+        res.status(401).render("401/index.ejs");
+        return;
+      }
+      try {
+        const token = authHeader[1];
+        await verifyTokenViaJwt(token);
+      } catch (err) {
+        logger.error(err);
+        res.status(403).render("403/index.ejs");
+        return;
+      }
+      try {
+        logger.debug(
+          `Passed forward headers: X-Forwarded-Method=${req.headers["X-Forwarded-Method"]}, X-Forwarded-Proto=${req.headers["X-Forwarded-Proto"]}, X-Forwarded-Host=${req.headers["X-Forwarded-Host"]}, X-Forwarded-Uri=${req.headers["X-Forwarded-Uri"]}, X-Forwarded-For=${req.headers["X-Forwarded-For"]}`
+        );
+        req.method = req.headers["X-Forwarded-Method"] as string;
+        // req.hostname = req.headers["X-Forwarded-Host"] as string;
+        req.headers.host = req.headers["X-Forwarded-Host"] as string;
+        if (req.headers["X-Forwarded-Uri"]) {
+          req.url = String(new URL(req.headers["X-Forwarded-Uri"] as string));
+          res.redirect(req.url);
+        }
+        res.redirect("/_oauth-info");
+      } catch (err) {
+        return next(err);
+      }
+    } else if (LOGIN_WHEN_NO_TOKEN) {
+      if (req.headers["X-Forwarded-Proto"] !== "https") {
+        logger.warn(
+          "Your're using cookie authorization for insecure connection!"
+        );
+      }
+      const code_verifier = generators.codeVerifier();
+      const code_challenge = generators.codeChallenge(code_verifier);
+      loginCache.has(req.ip) ? loginCache.del(req.ip) : null;
+      loginCache.set(req.ip, code_verifier);
+
+      const authorizationUrl = oidcClient.authorizationUrl({
+        scope: "openid email profile",
+        // resource: 'https://my.api.example.com/resource/32178',
+        code_challenge,
+        code_challenge_method: "S256",
+      });
+
+      res.redirect(authorizationUrl);
       return;
-    } else if (authHeader[0] !== "Bearer" && authHeader[0] !== "bearer") {
+    } else {
       res.status(401).render("401/index.ejs");
       return;
     }
-    try {
-      const token = authHeader[1];
-      await verifyTokenViaJwt(token);
-
-      const content = await getJwkKeys();
-      res.status(200).json(content);
-
-      // req.method = req.headers["X-Forwarded-Method"] as string;
-      // req.hostname = req.headers["X-Forwarded-Host"] as string;
-      // if (req.headers["X-Forwarded-Uri"]) {
-      //   req.url = String(new URL(req.headers["X-Forwarded-Uri"] as string));
-      // }
-    } catch (err) {
-      logger.error(err);
-      res.status(403).render("403/index.ejs");
-      return;
-    }
-  } else if (LOGIN_WHEN_NO_TOKEN) {
-    const code_verifier = generators.codeVerifier();
-    const code_challenge = generators.codeChallenge(code_verifier);
-    loginCache.has(req.ip) ? loginCache.del(req.ip) : null;
-    loginCache.set(req.ip, code_verifier);
-
-    const authorizationUrl = oidcClient.authorizationUrl({
-      scope: "openid email profile",
-      // resource: 'https://my.api.example.com/resource/32178',
-      code_challenge,
-      code_challenge_method: "S256",
-    });
-
-    res.redirect(authorizationUrl);
-    return;
-  } else {
-    res.status(401).render("401/index.ejs");
-    return;
   }
-});
+);
 
+/**
+ * Redirect URL. After successfull login, returned authentication code is
+ * validated and if it passes a login session is created (shouldn't be use
+ * on production).
+ */
 app.get(
   "/_oauth",
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -151,6 +181,9 @@ app.get(
   }
 );
 
+/**
+ * Service info endpoint.
+ */
 app.get("/_oauth-info", (req: Request, res: Response): void => {
   const content = {
     service: process.env.APP_NAME || "cx-traefik-forward-auth",
@@ -167,6 +200,23 @@ app.get("/_oauth-info", (req: Request, res: Response): void => {
   res.status(200).json(content);
 });
 
+/**
+ * General error handling middleware. Mind that it should be used as the last.
+ */
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  logger.error(err);
+  res.status(500).render("500/index.ejs");
+};
+app.use(errorHandler);
+
+/**
+ * Launch application running few tests before.
+ */
 try {
   validateDotenvFile();
   if (LOGIN_WHEN_NO_TOKEN) {
@@ -191,6 +241,5 @@ try {
   });
 } catch (err) {
   logger.error(err);
-  // process.kill(process.pid, "SIGTERM");
   process.exit(1);
 }
