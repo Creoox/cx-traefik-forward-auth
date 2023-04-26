85b82149e574438dd472da950fbcd0675c8a75c7
and-ratajski <andrzej.ratajski@effiware.com>
2023-04-26 21:07:11 +0200

    Reversed status to 400 for /AUTH_ENDPOINT/token

--- app/src/app.ts
+++ app/src/app.ts
@@ -98,22 +98,24 @@ app.get(
 
     // /AUTH_ENDPOINT/token endpoint
     if (
+      !!req.headers["x-forwarded-uri"] &&
       (req.headers["x-forwarded-uri"] as string).includes(
         `${AUTH_ENDPOINT}/token`
       )
     ) {
-      res.status(200).render("token/index.ejs", {
+      res.status(400).render("token/index.ejs", {
         access_token: (req.session as LoginSession).access_token,
       });
       return;
     }
     // /AUTH_ENDPOINT/info endpoint
     else if (
+      !!req.headers["x-forwarded-uri"] &&
       (req.headers["x-forwarded-uri"] as string).includes(
         `${AUTH_ENDPOINT}/info`
       )
     ) {
-      res.status(200).json(getEnvInfo(PORT, LOGIN_WHEN_NO_TOKEN, isProdEnv));
+      res.status(400).json(getEnvInfo(PORT, LOGIN_WHEN_NO_TOKEN, isProdEnv));
       return;
     }
     // /AUTH_ENDPOINT endpoint
